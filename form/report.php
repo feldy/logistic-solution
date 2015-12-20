414b3023a9db34332bcff65cd56ff3d03d4cc95f
Feldy Yusuf <f3ldy10rpl@gmail.com>
2015-12-20 12:47:12 +0700

    fixing

--- form/report.php
+++ form/report.php
@@ -2,27 +2,39 @@
 	Report
 	<hr>
 </span>
+<?php 
+	$xxx = mysql_query("SELECT count(*), sum(berat_barang), sum(asuransi), sum(packing_kayu), sum((berat_barang*harga_per_kilo)+asuransi+packing_kayu) FROM m_pengiriman where MONTH(tanggal) = MONTH(now())") or die(mysql_error());
+	$yyy = mysql_fetch_array($xxx);
+
+	$xxx1 = mysql_query("SELECT tujuan, count(tujuan) as jml FROM m_pengiriman where MONTH(tanggal) = MONTH(now()) group by tujuan order by jml desc LIMIT 1") or die(mysql_error());
+	$yyy2 = mysql_fetch_array($xxx1);
+	
+?>
 <table width="100%" border="0">
 	<tr style="font-size: 130%;">
 		<td>Laporan Pengiriman Bulan ini<br /><br /></td>
 	</tr>
 	<tr>
 		<td>Pengiriman</td>
-		<td>: ? Pengiriman</td>
+		<td>: <?php echo $yyy[0]; ?> Pengiriman</td>
 	</tr><tr>
 		<td>Daerah Pengiriman Terbanyak</td>
-		<td>: ?</td>
+		<td>: <?php echo $yyy2[0]; ?>  Dengan <?php echo $yyy2[1]; ?> Pengiriman</td>
 	</tr><tr>
 		<td>Jumlah Berat Pengiriman</td>
-		<td>: Kg</td>
+		<td>: <?php echo $yyy[1]; ?> Kg</td>
 	</tr><tr>
 		<td>Jumlah Pemasukan Asuransi</td>
-		<td>: Rp. </td>
+		<td>: Rp. <?php echo number_format($yyy[2]); ?></td>
 	</tr><tr>
 		<td>Jumlah Pemasukan Packing</td>
-		<td>: Rp.</td>
+		<td>: Rp. <?php echo number_format($yyy[3]); ?></td>
 	</tr><tr>
 		<td>Jumlah Pemasukan</td>
-		<td>: Rp.</td>
+		<td>: Rp. <?php echo number_format($yyy[4]); ?></td>
+	</tr><tr>
+		<td>
+	    	<span style="font-size: 150%;color: whitesmoke;font-family: fantasy;"><a href="#" onclick="print()">Print</a></span>						
+		</td>
 	</tr>
 </table>
--- system/save.php
+++ system/save.php
@@ -5,15 +5,15 @@
 			$nomor = strtoupper(uniqid());
 
 			$tujuan = $tujuan;
-			$harga_per_kilo = $harga;
-			$berat_barang = $berat;
-			$asuransi = $biayaAsuransi;
-			$packing_kayu = $biaya;
-			$lama_hari = $lamaHari;
+			$harga_per_kilo = ($harga != "") ? $harga : 0;
+			$berat_barang = ($berat != "") ? $berat : 0;
+			$asuransi = ($biayaAsuransi != "") ? $biayaAsuransi : 0;
+			$packing_kayu = ($biaya != "") ? $biaya : 0;
+			$lama_hari = ($lamaHari != "") ? $lamaHari : 0;
 
 			$r = mysql_query("INSERT INTO m_pengiriman VALUES ('$id', now(), '$nomor', '$tujuan', $harga_per_kilo, $berat_barang, $asuransi, $packing_kayu, '$lama_hari')") or die(mysql_error());
 			if ($r) {
-				echo "<script>alert('Berhasil Menyimpan Data'); window.location.href = '?page=print?id=".$id."';</script>";
+				echo "<script>alert('Berhasil Menyimpan Data'); window.location.href = '?page=print&id=".$id."';</script>";
 			} else {
 				echo "<script>alert('Gagal Menyimpan Data');window.history.back();</script>";
 			}
